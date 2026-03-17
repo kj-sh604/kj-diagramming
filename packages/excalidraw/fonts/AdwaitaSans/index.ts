aab241fcea12057c1f15f92306304bf91f817d37
kj_sh604 <43.splash@gmail.com>
2026-03-17 00:46:04 -0400

    refactor: packages/excalidraw/fonts/AdwaitaSans/

---
+++ packages/excalidraw/fonts/AdwaitaSans/index.ts
@@ -0,0 +1,9 @@
+import AdwaitaSansRegular from "./AdwaitaSans-Regular.woff2";
+
+import { type ExcalidrawFontFaceDescriptor } from "../Fonts";
+
+export const AdwaitaSansFontFaces: ExcalidrawFontFaceDescriptor[] = [
+  {
+    uri: AdwaitaSansRegular,
+  },
+];
