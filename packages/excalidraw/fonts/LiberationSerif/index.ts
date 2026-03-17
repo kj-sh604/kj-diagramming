c3654f810102d97cda66f41f2a074af88c05e97f
kj_sh604 <43.splash@gmail.com>
2026-03-17 00:46:04 -0400

    refactor: packages/excalidraw/fonts/LiberationSerif/

---
+++ packages/excalidraw/fonts/LiberationSerif/index.ts
@@ -0,0 +1,9 @@
+import LiberationSerifRegular from "./LiberationSerif-Regular.woff2";
+
+import { type ExcalidrawFontFaceDescriptor } from "../Fonts";
+
+export const LiberationSerifFontFaces: ExcalidrawFontFaceDescriptor[] = [
+  {
+    uri: LiberationSerifRegular,
+  },
+];
