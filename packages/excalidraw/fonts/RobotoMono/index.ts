ea110e1fdcd1e8d636b1c86cfbff9fb98684fe89
kj_sh604 <43.splash@gmail.com>
2026-03-17 00:46:04 -0400

    refactor: packages/excalidraw/fonts/RobotoMono/

---
+++ packages/excalidraw/fonts/RobotoMono/index.ts
@@ -0,0 +1,9 @@
+import RobotoMonoRegular from "./RobotoMono-Regular.woff2";
+
+import { type ExcalidrawFontFaceDescriptor } from "../Fonts";
+
+export const RobotoMonoFontFaces: ExcalidrawFontFaceDescriptor[] = [
+  {
+    uri: RobotoMonoRegular,
+  },
+];
