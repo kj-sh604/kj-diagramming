569600e575d88337d2c5d9901de6b77bedb21eaf
kj_sh604 <43.splash@gmail.com>
2026-03-17 00:46:04 -0400

    refactor: packages/excalidraw/fonts/JetBrainsMono/

---
+++ packages/excalidraw/fonts/JetBrainsMono/index.ts
@@ -0,0 +1,9 @@
+import JetBrainsMonoRegular from "./JetBrainsMono-Regular.woff2";
+
+import { type ExcalidrawFontFaceDescriptor } from "../Fonts";
+
+export const JetBrainsMonoFontFaces: ExcalidrawFontFaceDescriptor[] = [
+  {
+    uri: JetBrainsMonoRegular,
+  },
+];
