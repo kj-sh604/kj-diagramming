3405a99e48dbf016c66665566fbe102b5dadce7e
kj_sh604 <43.splash@gmail.com>
2026-03-17 00:46:04 -0400

    refactor: packages/excalidraw/fonts/LiberationMono/

---
+++ packages/excalidraw/fonts/LiberationMono/index.ts
@@ -0,0 +1,9 @@
+import LiberationMonoRegular from "./LiberationMono-Regular.woff2";
+
+import { type ExcalidrawFontFaceDescriptor } from "../Fonts";
+
+export const LiberationMonoFontFaces: ExcalidrawFontFaceDescriptor[] = [
+  {
+    uri: LiberationMonoRegular,
+  },
+];
